a4e85ba87271468fea9165d71d921599174acd60
f21 <957339173@qq.com>
2016-06-22 22:25:23 +0800

    this: JUnits工程注释完毕 next: 已有工程AndroidUnit书写 PS: 我要回来了,去尼玛的公司

--- 单元测试/Junit/src/test/junit/AdderTest.java
+++ 单元测试/Junit/src/test/junit/AdderTest.java
@@ -5,6 +5,15 @@ import com.junit.AdderImpl;
 import android.annotation.SuppressLint;
 import junit.framework.TestCase;
 
+/**
+ * 设置环境:
+ * <工程右键,properties> -- <Java Build Path; Libraries; Add Library> -- <Junit> -- <JUnit4> -- ok finish
+ * 运行工程：
+ * <工程右键.run as> -- <Junit Test; user configuration specific setting; Android Junit Test> -- ok finish
+ * @author YLine
+ *
+ * 2016年6月22日 下午10:16:27
+ */
 public class AdderTest extends TestCase
 {
     private AdderImpl adder;
