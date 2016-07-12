193aa49579b058b211abe8d4b5f0b7654a2e8eb7
f21 <957339173@qq.com>
2016-07-12 23:59:42 +0800

    this:sdcard 新建工程 next:sdcard 工程继续

---
+++ 数据传输/SDCard/src/com/sdcard/activity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.sdcard.activity;
+
+import com.sdcard.R;
+import com.yline.base.BaseActivity;
+import com.yline.log.LogFileUtil;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends BaseActivity
+{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        findViewById(R.id.btn_sdcard).setOnClickListener(new View.OnClickListener()
+        {
+            
+            @Override
+            public void onClick(View v)
+            {
+                LogFileUtil.v(MainApplication.TAG, "onClick -> btn_sdcard");
+            }
+        });
+    }
+    
+}
