9a52901e57b0cc3a89f76a43f03168fd114bdf33
f21 <957339173@qq.com>
2016-07-12 22:23:25 +0800

    this:整理到代理模式 next:continue

---
+++ 设计模式/代理模式Notification/src/com/dm/proxy/notification/subject/NotifyBig.java
@@ -0,0 +1,37 @@
+package com.dm.proxy.notification.subject;
+
+import com.dm.proxy.notification.R;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+public class NotifyBig extends Notify
+{
+    
+    public NotifyBig(Context context)
+    {
+        super(context);
+    }
+    
+    @SuppressLint("NewApi")
+    @Override
+    public void send()
+    {
+        Notification n = builder.build();
+        
+        n.contentView = new RemoteViews(context.getPackageName(), R.layout.remote_notify_proxy_normal);
+        n.bigContentView = new RemoteViews(context.getPackageName(), R.layout.remote_notify_proxy_big);
+        //	n.headsUpContentView = ;  5.0以上才有
+        
+        nm.notify(0, n);
+    }
+    
+    @Override
+    public void cancel()
+    {
+        nm.cancel(0);
+    }
+    
+}
---
+++ 设计模式/代理模式UML/src/com/dm/proxy/uml/activity/MainApplication.java
@@ -0,0 +1,25 @@
+package com.dm.proxy.uml.activity;
+
+import android.os.Message;
+
+import com.yline.application.AppConfig;
+import com.yline.application.BaseApplication;
+
+public class MainApplication extends BaseApplication
+{
+    public static final String TAG = "proxy_uml";
+    
+    @Override
+    protected void handlerDefault(Message msg)
+    {
+        
+    }
+    
+    @Override
+    protected AppConfig initConfig()
+    {
+        AppConfig appConfig = new AppConfig();
+        appConfig.setFileLogPath("代理模式UML"); // 默认开启日志,并写到文件中
+        return appConfig;
+    }
+}
