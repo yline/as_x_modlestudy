550256f9944d6eb982f7312493ee5e5c0fff1c21
f21 <957339173@qq.com>
2016-07-15 19:50:34 +0800

    this:SDCardUtil初步完成 next:暂无

--- 数据传输/SDCard/src/com/sdcard/activity/MainActivity.java
+++ 数据传输/SDCard/src/com/sdcard/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sdcard.activity;
 
 import com.sdcard.R;
+import com.sdcard.utils.SDCardUtil;
 import com.yline.base.BaseActivity;
 import com.yline.log.LogFileUtil;
 
@@ -23,6 +24,20 @@ public class MainActivity extends BaseActivity
             public void onClick(View v)
             {
                 LogFileUtil.v(MainApplication.TAG, "onClick -> btn_sdcard");
+                
+                LogFileUtil.i(MainApplication.TAG, "SDCardUtil.isSDCardEnable() = " + SDCardUtil.isSDCardEnable());
+                
+                LogFileUtil.i(MainApplication.TAG,
+                    "SDCardUtil.getRootDirectoryPath() = " + SDCardUtil.getRootDirectoryPath());
+                LogFileUtil.i(MainApplication.TAG, "SDCardUtil.getSDCardPath() = " + SDCardUtil.getSDCardPath());
+                
+                LogFileUtil.i(MainApplication.TAG,
+                    "SDCardUtil.getSDCardAvailableSize() = "
+                        + SDCardUtil.getSDCardAvailableSize() * 1.0f / (1024 * 1024 * 1024));
+                LogFileUtil.i(MainApplication.TAG,
+                    "SDCardUtil.getSDCardBlockSize() = "
+                        + SDCardUtil.getSDCardBlockSize() * 1.0f / (1024 * 1024 * 1024));
+                
             }
         });
     }
