fc4e536f4aae89e93dd80ae7e1bb9cbcb603eeb0
f21 <957339173@qq.com>
2016-07-17 19:15:43 +0800

    this:GalleryCarousel start next:complete it

---
+++ View/Gallery&Carousel/src/com/gallery/carousel/MainActivity.java
@@ -0,0 +1,17 @@
+package com.gallery.carousel;
+
+import com.yline.base.BaseActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity
+{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
