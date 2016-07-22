669eb71276922c56c2d150676b9178d89fdf34d3
f21 <957339173@qq.com>
2016-07-22 23:05:41 +0800

    this:UDP新建客户端工程和服务器工程 next:继续

---
+++ 数据传输/UDPDemo/src/com/udp/demo/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.udp.demo.activity;
+
+import com.udp.demo.R;
+import com.yline.base.BaseActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity
+{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
---
+++ 数据传输/UDPJavaServer/src/com/udp/server/Main.java
@@ -0,0 +1,9 @@
+package com.udp.server;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
