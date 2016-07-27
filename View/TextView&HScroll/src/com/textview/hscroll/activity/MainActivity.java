9733ba12b3bb525aa23d331e166f018a1d75e36f
f21 <957339173@qq.com>
2016-07-27 22:48:24 +0800

    this:TextView&HScroll next:暂无

---
+++ View/TextView&HScroll/src/com/textview/hscroll/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.textview.hscroll.activity;
+
+import com.textview.hscroll.R;
+import com.yline.base.BaseActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity
+{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
