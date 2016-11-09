e113ca762204c3b534d9741a6e2e2059a0d9309b
yline <957339173@qq.com>
2016-11-10 02:05:34 +0800

    this:ServiceSample finish next:continue

--- System/Service/ServiceSample/src/main/java/com/system/service/activity/MainActivity.java
+++ System/Service/ServiceSample/src/main/java/com/system/service/activity/MainActivity.java
@@ -1,9 +1,11 @@
 package com.system.service.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.system.service.R;
 import com.yline.base.BaseAppCompatActivity;
+import com.yline.log.LogFileUtil;
 
 public class MainActivity extends BaseAppCompatActivity
 {
@@ -13,5 +15,15 @@ public class MainActivity extends BaseAppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		findViewById(R.id.btn_activity_service_start).setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				LogFileUtil.v(MainApplication.TAG, "onClick btn_activity_service_start");
+				ServiceActivity.actionStart(MainActivity.this);
+			}
+		});
 	}
 }
