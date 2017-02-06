839260d6a949319613a8e40a051881abb900debb
yline <957339173@qq.com>
2017-02-06 16:42:14 +0800

    this:http album start next:Samplehttp

---
+++ DataNet/Http/HttpAlbum/src/main/java/com/http/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.http.activity;
+
+import android.os.Bundle;
+
+import com.http.R;
+import com.yline.base.BaseAppCompatActivity;
+
+/**
+ * Created by yline on 2017/2/6.
+ */
+public class MainActivity extends BaseAppCompatActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
