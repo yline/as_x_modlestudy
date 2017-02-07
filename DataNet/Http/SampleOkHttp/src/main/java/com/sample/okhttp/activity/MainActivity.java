270730513080a5bbdb24380d32ed3fcc1314efdd
yline <957339173@qq.com>
2017-02-07 19:16:32 +0800

    this:okHttp 告一段落 next:continue, 没有后台，学了后台继续来

--- DataNet/Http/SampleOkHttp/src/main/java/com/sample/okhttp/activity/MainActivity.java
+++ DataNet/Http/SampleOkHttp/src/main/java/com/sample/okhttp/activity/MainActivity.java
@@ -4,17 +4,8 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.sample.okhttp.R;
-import com.sample.okhttp.application.IApplication;
+import com.sample.okhttp.http.HttpHelper;
 import com.yline.base.BaseAppCompatActivity;
-import com.yline.log.LogFileUtil;
-
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 /**
  * Created by yline on 2017/2/7.
@@ -32,32 +23,10 @@ public class MainActivity extends BaseAppCompatActivity
 			@Override
 			public void onClick(View v)
 			{
-				OkHttpClient okHttpClient = new OkHttpClient();
-
 				// String httpUrl = "http://120.92.77.154/crest/index.php/api/example/users/N#887a19d10a6601b2";
 				String httpUrl = "https://github.com/yissan/CalendarView";
-				final Request request = new Request.Builder().url(httpUrl).build();
 
-				Call call = okHttpClient.newCall(request);
-				// 异步调度,两个回调是在 子线程中执行的
-				call.enqueue(new Callback()
-				{
-					@Override
-					public void onFailure(Call call, IOException e)
-					{
-						LogFileUtil.e("", "", e);
-					}
-					
-					@Override
-					public void onResponse(Call call, Response response) throws IOException
-					{
-						String htmlStr = response.body().string();
-						LogFileUtil.v(htmlStr);
-						IApplication.toast(htmlStr);
-
-						LogFileUtil.v(response.headers().toString());
-					}
-				});
+				HttpHelper.doGet(httpUrl);
 			}
 		});
 	}
