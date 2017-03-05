54159d1bebea538f05ce1eb7b89122e44aecf741
yline <957339173@qq.com>
2017-03-05 22:04:40 +0800

    this:style更新文档 next:continue

--- View/_Theory/Style&Theme/src/main/java/com/theory/sample/activity/MainApplication.java
+++ View/_Theory/Style&Theme/src/main/java/com/theory/sample/activity/MainApplication.java
@@ -3,9 +3,6 @@ package com.theory.sample.activity;
 import com.yline.application.BaseApplication;
 import com.yline.application.SDKConfig;
 
-/**
- * Created by yline on 2016/10/29.
- */
 public class MainApplication extends BaseApplication
 {
 	public static final String TAG = "Style&Theme";
