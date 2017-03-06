0511593d7893962aa8de5594c9fefd218da2ada8
yline <957339173@qq.com>
2017-03-06 12:50:51 +0800

    this:Http告一段落，下次采用连缀写法 next:DropDownMenu

--- DataNet/Http/SampleOkHttp/src/main/java/com/sample/okhttp/application/IApplication.java
+++ DataNet/Http/SampleOkHttp/src/main/java/com/sample/okhttp/application/IApplication.java
@@ -3,9 +3,6 @@ package com.sample.okhttp.application;
 import com.yline.application.BaseApplication;
 import com.yline.application.SDKConfig;
 
-/**
- * Created by yline on 2017/2/7.
- */
 public class IApplication extends BaseApplication
 {
 	public static final String TAG = "OkHttp";
