7c689ecd96f9e79cb89b95a6d2ff3da93417f446
yline <957339173@qq.com>
2017-04-18 15:22:29 +0800

    this:NoHttp首页 over next:开始对应的每一个案例

---
+++ DataNet/Http/SampleNoHttp/src/main/java/com/nohttp/activity/MainActivity.java
@@ -0,0 +1,205 @@
+package com.nohttp.activity;
+
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.nohttp.R;
+import com.yline.base.BaseAppCompatActivity;
+import com.yline.common.CommonRecyclerAdapter;
+import com.yline.common.CommonRecyclerViewHolder;
+import com.yline.utils.UIResizeUtil;
+import com.yline.utils.UIScreenUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends BaseAppCompatActivity
+{
+	private LinearLayout llTitle;
+
+	private ImageView ivTitle;
+
+	private int leftMarginDistance;
+
+	private AppBarLayout appBarLayout;
+
+	private MainRecyclerAdapter mainRecyclerAdapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		leftMarginDistance = UIScreenUtil.dp2px(this, 42);
+		llTitle = (LinearLayout) findViewById(R.id.toolbar_root);
+		ivTitle = (ImageView) findViewById(R.id.iv_toolbar_head);
+		appBarLayout = (AppBarLayout) findViewById(R.id.app_bar_layout);
+
+		UIResizeUtil.build().setIsWidthAdapter(false).setLeftMargin(-leftMarginDistance).commit(llTitle);
+		appBarLayout.addOnOffsetChangedListener(new OffSetChangedListener());
+
+		RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_start_activity);
+		recyclerView.setLayoutManager(new LinearLayoutManager(this));
+		recyclerView.addItemDecoration(new RecyclerView.ItemDecoration()
+		{
+			@Override
+			public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state)
+			{
+				outRect.set(10, 10, 10, 10);
+			}
+		});
+		recyclerView.setNestedScrollingEnabled(true);
+
+		mainRecyclerAdapter = new MainRecyclerAdapter();
+		recyclerView.setAdapter(mainRecyclerAdapter);
+
+		List<MainRecyclerBean> dataList = new ArrayList<>();
+		String[] titles = getResources().getStringArray(R.array.activity_start_items);
+		String[] titlesDes = getResources().getStringArray(R.array.activity_start_items_des);
+		for (int i = 0; i < titles.length; i++)
+		{
+			dataList.add(new MainRecyclerBean(titles[i], titlesDes[i]));
+		}
+		mainRecyclerAdapter.setDataList(dataList);
+	}
+
+	private class OffSetChangedListener implements AppBarLayout.OnOffsetChangedListener
+	{
+
+		@Override
+		public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset)
+		{
+			int maxScroll = appBarLayout.getTotalScrollRange();
+			float percentage = (float) Math.abs(verticalOffset) / (float) maxScroll;
+			ivTitle.setAlpha(percentage);
+
+			int background = (int) (250 * percentage);
+			llTitle.getBackground().mutate().setAlpha(background);
+
+			int realSize = (int) (leftMarginDistance * percentage);
+			llTitle.setTranslationX(realSize);
+		}
+	}
+
+	private void onItemViewClick(int position)
+	{
+		/*Intent intent = null;
+		switch (position)
+		{
+			case 0:// 最原始使用方法
+				intent = new Intent(this, OriginalActivity.class);
+				break;
+			case 1:// 各种请求方法演示(GET, POST, HEAD, PUT等等)
+				intent = new Intent(this, MethodActivity.class);
+				break;
+			case 2:// 请求图片
+				intent = new Intent(this, ImageActivity.class);
+				break;
+			case 3:// JsonObject, JsonArray
+				intent = new Intent(this, JsonActivity.class);
+				break;
+			case 4:// POST一段JSON、XML，自定义包体等
+				intent = new Intent(this, PostBodyActivity.class);
+				break;
+			case 5:// 自定义请求FastJson
+				intent = new Intent(this, DefineRequestActivity.class);
+				break;
+			case 6:// NoHttp缓存演示
+				intent = new Intent(this, CacheActivity.class);
+				break;
+			case 7:// 响应码302/303重定向演示
+				intent = new Intent(this, RedirectActivity.class);
+				break;
+			case 8:// 文件上传
+				intent = new Intent(this, UploadFileActivity.class);
+				break;
+			case 9: // 文件下载
+				intent = new Intent(this, DownloadActivity.class);
+				break;
+			case 10:// 如何取消请求
+				intent = new Intent(this, CancelActivity.class);
+				break;
+			case 11:// 同步请求
+				intent = new Intent(this, SyncActivity.class);
+				break;
+			case 12:// 通过代理服务器请求
+				intent = new Intent(this, ProXYActivity.class);
+				break;
+			case 13:// https请求
+				intent = new Intent(this, HttpsActivity.class);
+				break;
+			default:
+				break;
+		}
+		if (intent != null)
+		{
+			startActivity(intent);
+		}*/
+	}
+
+	private class MainRecyclerAdapter extends CommonRecyclerAdapter<MainRecyclerBean>
+	{
+
+		@Override
+		public void onBindViewHolder(CommonRecyclerViewHolder viewHolder, final int position)
+		{
+			viewHolder.setText(R.id.item_list_title, sList.get(position).getTitle());
+			viewHolder.setText(R.id.item_list_title_sub, sList.get(position).getSubTitle());
+
+			viewHolder.getItemView().setOnClickListener(new View.OnClickListener()
+			{
+				@Override
+				public void onClick(View v)
+				{
+					onItemViewClick(position);
+				}
+			});
+		}
+
+		@Override
+		public int getItemRes()
+		{
+			return R.layout.item_main;
+		}
+	}
+
+	private class MainRecyclerBean
+	{
+		private String title;
+
+		private String subTitle;
+
+		public MainRecyclerBean(String title, String subTitle)
+		{
+			this.title = title;
+			this.subTitle = subTitle;
+		}
+
+		public String getTitle()
+		{
+			return title;
+		}
+
+		public void setTitle(String title)
+		{
+			this.title = title;
+		}
+
+		public String getSubTitle()
+		{
+			return subTitle;
+		}
+
+		public void setSubTitle(String subTitle)
+		{
+			this.subTitle = subTitle;
+		}
+	}
+}
--- DataNet/Http/SampleOkHttp/src/main/java/com/sample/okhttp/fragment/TestGetFragment.java
+++ DataNet/Http/SampleOkHttp/src/main/java/com/sample/okhttp/fragment/TestGetFragment.java
@@ -1,11 +1,13 @@
 package com.sample.okhttp.fragment;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import com.sample.okhttp.http.XHttpAdapter;
 import com.sample.okhttp.http.XHttpUtil;
+import com.yline.log.LogFileUtil;
 import com.yline.test.BaseTestFragment;
 
 import java.util.HashMap;
@@ -35,6 +37,48 @@ public class TestGetFragment extends BaseTestFragment
 						tvShow.setText(s);
 					}
 				});
+			}
+		});
+
+		addButton("AsyncTask", new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				for (int i = 0; i < 100; i++)
+				{
+					final int finalI = i;
+					new AsyncTask<String, String, String>()
+					{
+						@Override
+						protected void onPreExecute()
+						{
+							super.onPreExecute();
+							LogFileUtil.v(finalI + "onPreExecute");
+						}
+
+						@Override
+						protected String doInBackground(String... params)
+						{
+							LogFileUtil.v(finalI + "doInBackground");
+							try
+							{
+								Thread.sleep(20 * 1000);
+							} catch (InterruptedException e)
+							{
+								e.printStackTrace();
+							}
+							return null;
+						}
+
+						@Override
+						protected void onPostExecute(String s)
+						{
+							LogFileUtil.v(finalI + "doInBackground");
+							super.onPostExecute(s);
+						}
+					}.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "");
+				}
 			}
 		});
 	}
