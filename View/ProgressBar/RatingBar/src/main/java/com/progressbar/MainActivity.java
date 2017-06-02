78a9c73fa0c607ee0ce4e39306abb5122a228ab3
yline <957339173@qq.com>
2017-06-02 11:12:18 +0800

    this:ProgressBar RatingBar next:continue

---
+++ View/ProgressBar/RatingBar/src/main/java/com/progressbar/MainActivity.java
@@ -0,0 +1,29 @@
+package com.progressbar;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+
+import com.yline.application.SDKManager;
+import com.yline.base.BaseAppCompatActivity;
+
+public class MainActivity extends BaseAppCompatActivity
+{
+	private RatingBar ratingBar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		ratingBar = (RatingBar) findViewById(R.id.rating_bar);
+		ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener()
+		{
+			@Override
+			public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser)
+			{
+				SDKManager.toast("rating = " + rating + ", fromUser = " + fromUser);
+			}
+		});
+	}
+}
