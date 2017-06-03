a1abd8898d1f41837b88989c02851dffdc3a5be5
yline <957339173@qq.com>
2017-06-03 11:14:35 +0800

    this:LineProgressBar next:continue

---
+++ View/ProgressBar/LineProgressBar/src/main/java/com/progressbar/line/MainActivity.java
@@ -0,0 +1,16 @@
+package com.progressbar.line;
+
+import android.os.Bundle;
+
+import com.yline.base.BaseAppCompatActivity;
+
+public class MainActivity extends BaseAppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
