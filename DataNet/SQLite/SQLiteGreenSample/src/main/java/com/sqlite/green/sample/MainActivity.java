9ee652f948b1437648354f759ce9d224f61fdf52
yline <957339173@qq.com>
2017-09-14 20:28:33 +0800

    this:补上，基本使用方法 next:数据库告一段落，frescro调研开始

--- DataNet/SQLite/SQLiteGreenSample/src/main/java/com/sqlite/green/sample/MainActivity.java
+++ DataNet/SQLite/SQLiteGreenSample/src/main/java/com/sqlite/green/sample/MainActivity.java
@@ -3,16 +3,33 @@ package com.sqlite.green.sample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.sqlite.green.DaoManager;
+import com.sqlite.green.async.AsyncOperation;
+import com.sqlite.green.async.AsyncOperationListener;
+import com.sqlite.green.async.AsyncOperationModel;
+import com.sqlite.green.test.NetCacheModel;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
+
+        // 第一步
         DaoManager.init(this);
 
-        DaoManager.getNetCacheModelDao().insert(new NetCacheModel("", ""));
-        DaoManager.getNetCacheModelDaoAsync().insert(new NetCacheModel("", ""));*/
+        // UI线程操作
+        DaoManager.getNetCacheModelDao().insert(new NetCacheModel("url", new Object()));
+
+        // 异步操作
+        AsyncOperation asyncOperation = DaoManager.getNetCacheModelDaoAsync();
+        asyncOperation.insert(new NetCacheModel("url", new Object()));
+        asyncOperation.setOnOperationListener(new AsyncOperationListener() {
+            @Override
+            public void onAsyncCompleted(AsyncOperationModel operationModel) {
+                // 非UI线程，依旧在子线程之中
+            }
+        });
     }
 }
