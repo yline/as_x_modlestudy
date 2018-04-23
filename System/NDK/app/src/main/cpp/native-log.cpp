d1c5f265e2faeba5d8d93a0a11dbd9cb7ed142ed
yline <957339173@qq.com>
2018-04-23 19:44:56 +0800

    this:解决 无法调用android log问题 next:continue

--- System/NDK/app/src/main/cpp/native-log.cpp
+++ System/NDK/app/src/main/cpp/native-log.cpp
@@ -1,11 +1,14 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
 extern "C"
 JNIEXPORT jstring
 
 JNICALL
 Java_com_ndk_jni_JniManager_logByJni(JNIEnv *env, jobject) {
+    __android_log_print(ANDROID_LOG_INFO, "Jni Msg", "fuck");
+
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
