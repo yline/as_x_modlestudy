ca6576005bb167034adce5f8be254c7b5fa66ad5
yline <957339173@qq.com>
2018-04-24 14:36:10 +0800

    this:单独调用一个 方法完成 next:调用第二个方法，并设置全局变量

--- System/NDK/app/src/main/cpp/native-log.cpp
+++ System/NDK/app/src/main/cpp/native-log.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <android/log.h>
 
+#define TAG "xxx-doProvider"
+#define TAG_STATIC "xxx-doStaticProvider"
+
 extern "C" {
 
 /**
@@ -9,7 +12,32 @@ extern "C" {
  * @param env
  */
 void Java_com_ndk_jni_JniProvider_doProvider(JNIEnv *env, jobject) {
-    __android_log_print(ANDROID_LOG_INFO, "xxx-", "doProvider start");
+    __android_log_print(ANDROID_LOG_INFO, TAG, "start");
+
+    if (NULL == env) {
+        __android_log_print(ANDROID_LOG_INFO, TAG, "env is null");
+        return;
+    }
+
+    jclass clazz = env->FindClass("com/ndk/jni/JniProvider");
+    if (NULL == clazz) {
+        __android_log_print(ANDROID_LOG_INFO, TAG, "clazz is null");
+        return;
+    }
+
+    jmethodID method = env->GetMethodID(clazz, "testProvider", "()Ljava/lang/String;");
+    if (NULL == method) {
+        __android_log_print(ANDROID_LOG_INFO, TAG, "method is null");
+        return;
+    }
+
+    jobject obj = env->AllocObject(clazz);
+    if (NULL == obj) {
+        __android_log_print(ANDROID_LOG_INFO, TAG, "obj is null");
+        return;
+    }
+
+    env->CallObjectMethod(obj, method);
 }
 
 /**
@@ -17,7 +45,7 @@ void Java_com_ndk_jni_JniProvider_doProvider(JNIEnv *env, jobject) {
  * @param env
  */
 void Java_com_ndk_jni_JniProvider_doStaticProvider(JNIEnv *env, jobject) {
-    __android_log_print(ANDROID_LOG_INFO, "xxx-", "doStaticProvider start");
+    __android_log_print(ANDROID_LOG_INFO, TAG_STATIC, "start");
 }
 
 }
