77ff0a630907db676fc5b96b534c2a93ac1cae8d
ylineWork <linjiang@kjtpay.local>
2019-06-27 19:22:55 +0800

    this:推送到达率开始

---
+++ System/Process/KeepAliveProcess/src/main/java/com/yline/process/alive/MainApplication.java
@@ -0,0 +1,11 @@
+package com.yline.process.alive;
+
+import com.yline.application.BaseApplication;
+
+public class MainApplication extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
